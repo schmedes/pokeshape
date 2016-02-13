9a3eb1e681bd56bd1f3f15a6ed8cd63282cd6f6a
schmedes <julian.schmedes@gmx.de>
2016-02-13 20:33:22 +0100

    adding base functionality

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pokemon guessing game based on their shapes
+
+## TODO-List
+
+- styling
+- adding a score
+- adding timer
+- adding failure message
+- adding hints from pokeapi.co
+- adding share-buttons to share their score
+
--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,1 +1,45 @@
+require('../styles/main.css');
+const pokedex = require('json!./pokedex.json');
 
+const lastGuessed = [];
+const imageContainer = document.querySelector('.shape-wrapper');
+const image = document.querySelector('.shape');
+const guess = document.querySelector('#guess');
+
+
+function getPokemon() {
+    const pokemon = Math.floor(Math.random() * (151 - 1 +1)) + 1;
+    let guessedAlready = false;
+    lastGuessed.forEach((el)=>{
+       if(el == pokemon) {
+           guessedAlready = true;
+       } 
+    });
+    if(guessedAlready) {
+        console.log('oldpokemon');
+        return getPokemon();
+    } else {
+  return Math.floor(Math.random() * (151 - 1 +1)) + 1;
+    }
+}
+
+function renderImage(pokemonId) {
+  image.src = `../assets/${pokemonId}.png`;
+}
+
+renderImage(getPokemon());
+
+guess.addEventListener('keypress', (event)=>{
+   if(event.which === 13) {
+       if(pokedex[image.src.split("/").pop().split(".").shift()] === event.target.value.toUpperCase()){
+           image.classList.toggle('darken');
+           setTimeout(()=>{
+           console.log('Congratz');
+           image.classList.toggle('darken');
+           renderImage(getPokemon()); 
+           }, 3000);
+           lastGuessed.push(image.src.split("/").pop().split(".").shift());
+           event.target.value = "";
+       }
+   } 
+});

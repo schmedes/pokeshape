ee36955b8f36c84cfad4a5d3f8469c92da0fd8be
schmedes <julian.schmedes@gmx.de>
2016-02-19 19:35:33 +0100

    define basecolors + add eslint parseroptions

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,46 +1,45 @@
 require('../styles/main.css');
 const pokedex = require('json!./pokedex.json');
 let pokemonLeft = [];
-const imageContainer = document.querySelector('.shape-wrapper');
 const image = document.querySelector('.shape');
 const guess = document.querySelector('#guess');
 const score = document.querySelector('.score');
-
-function initGame(){
-    score.innerHTML = 0;
-    for(let it = 1; it<=151; it++ ){
-        pokemonLeft.push(it+'');
-    }
-    renderImage(getPokemon());
-}
-
-function getPokemon() {
-    if(pokemonLeft) {
-    const pokemonNr = Math.floor(Math.random() * (pokemonLeft.length - 1));
-    console.log(pokemonLeft[pokemonNr])
-    const pokemon = pokemonLeft[pokemonNr];
-    pokemonLeft = [...pokemonLeft.slice(0, pokemonNr), ...pokemonLeft.slice(pokemonNr + 1, pokemonLeft.length)];
-    console.log(pokemonLeft);
-    return pokemon;
-    }
-}
 
 function renderImage(pokemonId) {
   image.src = `../assets/${pokemonId}.png`;
 }
 
+function getPokemon() {
+  if(pokemonLeft) {
+    const pokemonNr = Math.floor(Math.random() * (pokemonLeft.length - 1));
+    const pokemon = pokemonLeft[pokemonNr];
+    pokemonLeft = [...pokemonLeft.slice(0, pokemonNr),
+      ...pokemonLeft.slice(pokemonNr + 1, pokemonLeft.length)];
+    return pokemon;
+  }
+  return false;
+}
+
+function initGame() {
+  score.innerHTML = 0;
+  for(let it = 1; it <= 151; it++) {
+    pokemonLeft.push(it);
+  }
+  renderImage(getPokemon());
+}
+
 initGame();
 
-guess.addEventListener('keypress', (event)=>{
-   if(event.which === 13) {
-       if(pokedex[image.src.split("/").pop().split(".").shift()] === event.target.value.toUpperCase()){
-           image.classList.toggle('darken');
-           score.innerHTML = parseInt(score.innerHTML) + 1;
-           setTimeout(()=>{
-           image.classList.toggle('darken');
-           renderImage(getPokemon()); 
-           }, 2000);
-           event.target.value = "";
-       }
-   } 
+guess.addEventListener('keypress', (event) => {
+  if(event.which === 13) {
+    if(pokedex[image.src.split('/').pop().split('.').shift()] === guess.value.toUpperCase()) {
+      image.classList.toggle('darken');
+      score.innerHTML = parseInt(score.innerHTML, 10) + 1;
+      setTimeout(() => {
+        image.classList.toggle('darken');
+        renderImage(getPokemon());
+      }, 2000);
+      guess.value = '';
+    }
+  }
 });
